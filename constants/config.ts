d3288c2dd2c0059be76246e836886c9439de9cb8
iamyeasirarafat <yas.yeasir@gmail.com>
2026-09-25 00:41:04 +0600

    Update: switch API URL to production

--- constants/config.ts
+++ constants/config.ts
@@ -1,8 +1,8 @@
 export const APP_CONFIG = {
   name: 'Ybala Admin App',
   version: '1.0.0',
-  // apiUrl: "https://api.ybalawrqenab.com",
-  apiUrl: "http://192.168.0.115:5050",
+  apiUrl: "https://api.ybalawrqenab.com",
+  // apiUrl: "http://192.168.0.115:5050",
 };
 
 export const QUERY_CONFIG = {
